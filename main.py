47e643ee519cd642ab846109d09c39e18878e36f
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-02 18:21:09 +0900

    파일 이름 바꿈

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ from boxer import Boxer
 
 P1 = {
     "controls": "wasd",
-    "idle":  {"image":"player2/player2_Idle.png", "cols":10, "w":499, "h":489, "scale":0.5},
+    "idle":  {"image":"player1/player1_Idle.png", "cols":10, "w":499, "h":489, "scale":0.5},
     "spawn": {"x": 100, "y": 300, "face": 1},
-    "walk_backward": {"image":"player2/player2_Walk_Backward.png", "cols":10, "w": 499, "h":489, "scale":0.5},
-    "walk_forward":  {"image":"player2/player2_Walk_Forward.png",  "cols":10, "w": 499, "h":489, "scale":0.5}
+    "walk_backward": {"image":"player1/player1_Walk_Backward.png", "cols":10, "w": 499, "h":489, "scale":0.5},
+    "walk_forward":  {"image":"player1/player1_Walk_Forward.png",  "cols":10, "w": 499, "h":489, "scale":0.5}
 
 }
 
 P2 = {
     "controls": "arrows",
-    "idle":  {"image":"player1/player1_Idle.png", "cols":10, "w":744, "h":711, "scale":0.3},
+    "idle":  {"image":"player2/player2_Idle.png", "cols":10, "w":744, "h":711, "scale":0.3},
     "spawn": {"x": 700, "y": 300, "face": 1},
-    "walk_backward": {"image":"player1/player1_Walk_Backward.png", "cols": 10, "w":746, "h":713, "scale":0.3},
-    "walk_forward":  {"image":"player1/player1_Walk_Forward.png",  "cols": 10, "w":746, "h":713, "scale":0.3},
-    "FrontRightPunch": {"image":"player1/player1_FrontRightPunch.png",  "cols": 8, "w":180, "h":561, "scale":0.3},
-    "FrontRightUppercut": {"image":"player1/player1_FrontRightUppercut.png",  "cols": 8, "w":180, "h":561, "scale":0.3},
-    "FrontLeftPunch": {"image":"player1/player1_FrontLeftPunch.png",  "cols": 8, "w":180, "h":561, "scale":0.3}
+    "walk_backward": {"image":"player2/player2_Walk_Backward.png", "cols": 10, "w":746, "h":713, "scale":0.3},
+    "walk_forward":  {"image":"player2/player2_Walk_Forward.png",  "cols": 10, "w":746, "h":713, "scale":0.3},
+    "FrontRightPunch": {"image":"player2/player2_FrontRightPunch.png",  "cols": 8, "w":180, "h":561, "scale":0.3},
+    "FrontRightUppercut": {"image":"player2/player2_FrontRightUppercut.png",  "cols": 8, "w":180, "h":561, "scale":0.3},
+    "FrontLeftPunch": {"image":"player2/player2_FrontLeftPunch.png",  "cols": 8, "w":180, "h":561, "scale":0.3}
 }
 
 def handle_events():
