4453ba273f925a04aa3aef46155157f4dbe00071
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-20 06:33:57 +0900

    Update game_world.py
    remove_collision_object 함수 생성

--- game_world.py
+++ game_world.py
@@ -6,14 +6,6 @@ def add_object(o, depth):
 def add_objects(ol, depth):
     world[depth] += ol
 
-def remove_object(o):
-    for layer in world:
-        if o in layer:
-            layer.remove(o)
-            return
-
-    raise Exception("World 에 존재하지 않는 오브젝트를 지우려고 시도함")
-
 def update():
     for layer in world:
         for o in layer:
@@ -23,6 +15,9 @@ def render():
     for layer in world:
         for o in layer:
             o.draw()
+
+def remove_collision_object(o):
+    pass
 
 def clear():
     for layer in world:
