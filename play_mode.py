fcc6bed1f9334a0bdb82025896e76272fa5e38b2
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-20 08:39:34 +0900

    Update play_mode.py
    cfg 값

--- play_mode.py
+++ play_mode.py
@@ -5,6 +5,7 @@ import game_world
 
 P1 = {
     "controls": "wasd",
+    "bb": {"w": 0.25, "h": 0.35, "x_offset": -40, "y_offset": 0},
     "idle":  {"image":"player1/player1_Idle.png", "cols":10, "w":499, "h":489, "scale":0.5},
     "spawn": {"x": 100, "y": 300, "face": 1},
     "walk_backward": {"image":"player1/player1_Walk_Backward.png", "cols": 10, "w": 499, "h": 489, "scale": 0.5},
@@ -19,6 +20,7 @@ P1 = {
 
 P2 = {
     "controls": "arrows",
+    "bb": {"w": 0.30, "h": 0.40, "x_offset": -70, "y_offset": 0},
     "idle":  {"image":"player2/player2_Idle.png", "cols":10, "w":744, "h":711, "scale":0.3},
     "spawn": {"x": 700, "y": 300, "face": 1},
     "walk_backward": {"image":"player2/player2_Walk_Backward.png", "cols": 10, "w":746, "h":713, "scale":0.3},
