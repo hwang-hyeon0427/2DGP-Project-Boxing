729d9a9a88230c2c842c301cbb85adfd54bceab2
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-20 06:39:58 +0900

    Update game_world.py
    collide 함수 완성

--- game_world.py
+++ game_world.py
@@ -24,7 +24,15 @@ def clear():
         layer.clear()
 
 def collide(a, b):
-    pass
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True # 충돌 발생
 
 collision_pairs = {}
 def add_collision_pair(group, a, b):
