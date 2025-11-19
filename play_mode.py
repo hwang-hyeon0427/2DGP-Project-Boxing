5d576e25fa78793b04645b59288e69ac3682d7ad
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-20 07:24:29 +0900

    Update play_mode.py
    png 정보 추가

--- play_mode.py
+++ play_mode.py
@@ -7,11 +7,14 @@ P1 = {
     "controls": "wasd",
     "idle":  {"image":"player1/player1_Idle.png", "cols":10, "w":499, "h":489, "scale":0.5},
     "spawn": {"x": 100, "y": 300, "face": 1},
-    "walk_backward": {"image":"player1/player1_Walk_Backward.png", "cols":10, "w": 499, "h":489, "scale":0.5},
-    "walk_forward":  {"image":"player1/player1_Walk_Forward.png",  "cols":10, "w": 499, "h":489, "scale":0.5},
-    "front_hand": {"image":"player1/player1_FrontHand.png",  "cols":6, "w":499, "h":489, "scale":0.5},
-    "uppercut": {"image":"player1/player1_Uppercut.png",  "cols":7, "w":499, "h":489, "scale":0.5},
-    "rear_hand": {"image":"player1/player1_RearHand.png",  "cols":6, "w":499, "h":489, "scale":0.5}
+    "walk_backward": {"image":"player1/player1_Walk_Backward.png", "cols": 10, "w": 499, "h": 489, "scale": 0.5},
+    "walk_forward":  {"image":"player1/player1_Walk_Forward.png",  "cols": 10, "w": 499, "h": 489, "scale": 0.5},
+    "front_hand": {"image":"player1/player1_FrontHand.png",  "cols": 6, "w": 499, "h": 489, "scale": 0.5},
+    "uppercut": {"image":"player1/player1_Uppercut.png",  "cols": 7, "w": 499, "h": 489, "scale": 0.5},
+    "rear_hand": {"image":"player1/player1_RearHand.png",  "cols": 6, "w": 499, "h": 489, "scale": 0.5},
+    "blocking": {"image":"player1/player1_Blocking.png",  "cols": 10, "w": 499, "h": 489, "scale": 0.5},
+    "dizzy": {"image":"player1/player1_Dizzy.png",  "cols": 8, "w": 499, "h": 489, "scale": 0.5},
+    "ko": {"image":"player1/player1_KO.png",  "cols": 10, "w": 499, "h": 489, "scale": 0.5}
 }
 
 P2 = {
@@ -22,7 +25,10 @@ P2 = {
     "walk_forward":  {"image":"player2/player2_Walk_Forward.png",  "cols": 10, "w":746, "h":713, "scale":0.3},
     "front_hand": {"image":"player2/player2_FrontHand.png",  "cols": 8, "w":744, "h":713, "scale":0.3},
     "uppercut": {"image":"player2/player2_Uppercut.png",  "cols": 8, "w":744, "h":713, "scale":0.3},
-    "rear_hand": {"image":"player2/player2_RearHand.png",  "cols": 8, "w":744, "h":713, "scale":0.3}
+    "rear_hand": {"image":"player2/player2_RearHand.png",  "cols": 8, "w":744, "h":713, "scale":0.3},
+    "blocking": {"image":"player2/player2_Block.png",  "cols": 10, "w":744, "h":713, "scale":0.3},
+    "dizzy": {"image":"player2/player2_Dizzy.png",  "cols": 10, "w":744, "h":711, "scale":0.3},
+    "ko": {"image":"player2/player2_KO.png",  "cols": 8, "w":744, "h":711, "scale":0.3}
 }
 
 def handle_events():
@@ -44,6 +50,7 @@ def init():
 
     p2 = Boxer(P2)
     game_world.add_object(p2, 1)
+
 
     game_world.add_collision_pair('boxer:boxer', p1, p2)
 
