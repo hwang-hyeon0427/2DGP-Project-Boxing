a03f7057c740529834d00dd62aed3f18c0eb4462
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-20 13:04:53 +0900

    Update hpbar.py
    draw 함수 완성

--- hpbar.py
+++ hpbar.py
@@ -7,10 +7,14 @@ class HBBar:
         self.y = y
         self.width = width
         self.height = height
+        self.boxer.max_hp = 100
 
     def update(self):
         pass
 
     def draw(self):
-        pass
+        ratio = max(self.boxer.hp / self.boxer.max_hp, 0)
+        draw_rectangle(self.x - self.width//2, self.y - self.height//2, self.x + self.width//2, self.y + self.height//2)
 
+        life_width = int(self.width * ratio)
+        draw_rectangle(self.x - self.width//2, self.y - self.height//2, self.x + self.width//2, self.y + self.height//2)
