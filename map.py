374754b1f868491ac725ddb36e1a9f4bbb2b8233
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-20 13:51:18 +0900

    Update map.py
    Map 클래스 생성

---
+++ map.py
@@ -0,0 +1,17 @@
+from pico2d import *
+
+class Map:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+    
+    def draw(self):
+        pass
+
+    def get_bb(self):
+        pass
+
+    def handle_collision(self, group, other):
+        pass
