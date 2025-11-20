70ce335323de5f6e27c886180caf4052a7197cae
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-20 13:59:51 +0900

    Update map.py
    파일 위치 오타 수정

--- map.py
+++ map.py
@@ -2,7 +2,7 @@ from pico2d import *
 
 class Map:
     def __init__(self):
-        self.image = load_image('boxingring.png')
+        self.image = load_image('background/boxingring.png')
 
     def update(self):
         pass
