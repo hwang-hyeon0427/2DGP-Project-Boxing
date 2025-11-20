a1a76a1683a7855561603de390431397a7968caf
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-20 13:00:16 +0900

    Update hpbar.py
    생성자 함수 완성

--- hpbar.py
+++ hpbar.py
@@ -1,8 +1,12 @@
 from pico2d import draw_rectangle, draw_line
 
 class HBBar:
-    def __init__(self):
-        pass
+    def __init__(self, boxer, x, y, width = 120, height = 10):
+        self.boxer = boxer
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
 
     def update(self):
         pass
