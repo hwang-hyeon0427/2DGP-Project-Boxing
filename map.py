a88d3975a6e7b0854f549450dcc3658eb7a9fa7e
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-21 18:46:40 +0900

    Update map.py

--- map.py
+++ map.py
@@ -1,15 +1,27 @@
 from pico2d import *
+import random
 
 class Map:
-    def __init__(self):
-        self.image = load_image('background/boxingring.png')
+    FRAME_W = 320
+    FRAME_H = 173
+    COLS = 4
+    ROWS = 4
+
+    def __init__(self, index=None):
+        self.image = load_image('background/Boxing_Rings.png')
+
+        if index is None:
+            index = random.randint(0, Map.COLS * Map.ROWS - 1)
+
+        self.index = index
+
+        self.col = index % Map.COLS
+        self.row = Map.ROWS - 1 - (index // Map.COLS)
 
     def update(self):
         pass
 
     def draw(self):
-        self.image.draw(400, 300)
-        draw_rectangle(*self.get_bb())
 
 
     def get_bb(self):
