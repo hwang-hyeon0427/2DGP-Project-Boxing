2a5188c348afbc1d3f5e6c820ba47961786c5177
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-23 19:12:16 +0900

    Update hitbox.py

--- hitbox.py
+++ hitbox.py
@@ -1,8 +1,14 @@
 from pico2d import *
 
 class HitBox:
-    def __init__(self):
-        pass
+    def __init__(self, owner, offset_x, offset_y, w, h, duration):
+        self.owner = owner
+        self.offset_x = offset_x
+        self.offset_y = offset_y
+        self.width = w
+        self.height = h
+        self.end_time = get_time() + duration
+
 
     def draw(self):
         pass
