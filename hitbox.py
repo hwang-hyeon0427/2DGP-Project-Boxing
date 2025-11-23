a19b822b0b22a09a2801b937c2d380d94d1531f4
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-23 19:10:00 +0900

    Update hitbox.py

---
+++ hitbox.py
@@ -0,0 +1,17 @@
+from pico2d import *
+
+class HitBox:
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
+
+    def get_bb(self):
+        return 0, 0, 0, 0
+
+    def handle_collision(self, other, group):
+        pass
