7cc6aa3c45ff27bfb6722ce3ecd0f71d8aed0dcf
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-24 14:43:43 +0900

    Update attack_state.py

--- attack_state.py
+++ attack_state.py
@@ -2,11 +2,13 @@ class AttackState:
     def __init__(self, boxer, attack_type):
         self.b = boxer
         self.type = attack_type
+        self.done = False
 
     def enter(self, e):
-        sheet = self.b.cfg.[self.type]
+        sheet = self.b.cfg[self.type]
         self.b.use_sheet(sheet)
         self.b.frame = 0
+        self.done = False
 
         self.b.is_attacking = True
         self.b.current_attack_type = self.type
@@ -16,11 +18,18 @@ class AttackState:
         self.b.current_attack_type = None
 
     def do(self):
-        if not self.done:
-            self.b.frame += 1
-            if self.b.frame >= self.b.cols:
-                self.done = True
-                self.b.state_machine.handle_state_event(('ANIMATION_END', None))
+        if self.done:
+            return
+
+        self.b.frame += 1
+
+        # 히트박스 생성 포인트 (예시: frame == 2 에 히트박스 생성)
+        if self.b.frame == 2:
+            self.b.spawn_hitbox()  # Boxer 클래스에 만들면 됨
+
+        if self.b.frame >= self.b.cols:
+            self.done = True
+            self.b.state_machine.handle_state_event(('ANIMATION_END', None))
 
     def draw(self):
         self.b.draw_current()
