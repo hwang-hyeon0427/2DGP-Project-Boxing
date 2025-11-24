bca311bfcc84f84594cbe6c2ec1935e2877654d8
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-25 01:46:21 +0900

    애니메이션 로고 방식 구현

--- logo_mode.py
+++ logo_mode.py
@@ -3,14 +3,21 @@ import game_framework
 import title_mode
 
 image = None
-logo_stop_time = 0
-w, h = 0, 0
+
+frame = 0.0
+FRAME_COUNT = 3
+
+FRAME_W = 826 //3
+FRAME_H = 272
+
+ANIMATTION_SPEED = 10.0
+logo_start_time = 0
 
 def init():
-    global image, logo_stop_time, w, h
-    image = load_image('tuk_credit.png')
-    logo_stop_time = get_time()
-    w, h = image.w, image.h
+    global image, logo_start_time, frame
+    image = load_image('SUPER-PUNCH-OUT_logo.png')
+    frame = 0.0
+    logo_start_time = get_time()
 
 
 def finish():
@@ -18,13 +25,22 @@ def finish():
     del image
 
 def update():
-    if get_time() - logo_stop_time > 2.0:
+    global frame
+    frame += ANIMATTION_SPEED * game_framework.frame_time
+    frame %= FRAME_COUNT
+
+    if get_time() - logo_start_time > 2.0:
         game_framework.change_mode(title_mode)
 
 def draw():
     clear_canvas()
-    scale = 1.5
-    image.draw(317*4//2, 128*6//2, w * scale, h * scale)
+    current_frame = int(frame)
+    src_x = current_frame * FRAME_W # 현재 프레임에 해당하는 이미지의 x 좌표
+    scale = 3
+
+    w, h = get_canvas_width(), get_canvas_height() # 캔버스 크기 가져오기
+    image.clip_draw(src_x, 0, FRAME_W, FRAME_H, w // 2, h // 2, FRAME_W * scale, FRAME_H * scale)  # clip_draw (src_x, src_y, src_w, src_h, x, y)
+
     update_canvas()
 
 def handle_events():
