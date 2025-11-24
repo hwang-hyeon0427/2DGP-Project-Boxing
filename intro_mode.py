7920b21f4ed8780cbd2921f233400d4ff5c3771a
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-25 02:05:18 +0900

    Update intro_mode.py

---
+++ intro_mode.py
@@ -0,0 +1,25 @@
+from pico2d import *
+import game_framework
+import title_mode
+
+image= None
+
+
+def init():
+    pass
+
+def finish():
+    pass
+
+def update():
+    pass
+
+def draw():
+    pass
+
+def handle_events():
+    pass
+
+def pause(): pass
+def pause(): pass
+def resume(): pass
