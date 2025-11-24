a122f5f55e47b875754cda8bb0ef1bc1e56b0745
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-25 02:20:57 +0900

    로고 -> 인트로 재생

--- logo_mode.py
+++ logo_mode.py
@@ -1,6 +1,6 @@
 from pico2d import *
 import game_framework
-import title_mode
+import intro_mode
 
 image = None
 
@@ -30,7 +30,7 @@ def update():
     frame %= FRAME_COUNT
 
     if get_time() - logo_start_time > 2.0:
-        game_framework.change_mode(title_mode)
+        game_framework.change_mode(intro_mode)
 
 def draw():
     clear_canvas()
