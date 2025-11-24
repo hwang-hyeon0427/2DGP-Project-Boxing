ea9e98275c3e8c40b419b413bf8e29f0355e10a2
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-24 14:43:53 +0900

    Update hitbox.py

--- hitbox.py
+++ hitbox.py
@@ -1,26 +1,36 @@
 from pico2d import *
 import game_world
+
 class HitBox:
     def __init__(self, owner, offset_x, offset_y, w, h, duration):
-        self.owner = owner
-        self.offset_x = offset_x
-        self.offset_y = offset_y
-        self.w = w
-        self.h = h
-        self.end_time = get_time() + duration
-
+        self.owner = owner          # 히트박스를 만든 주체(공격자)
+        self.offset_x = offset_x    # x 오프셋(얼굴 방향 따라 +/-)
+        self.offset_y = offset_y    # y 오프셋
+        self.w = w                  # 히트박스 너비
+        self.h = h                  # 히트박스 높이
+        self.end_time = get_time() + duration  # 일정 시간 지나면 제거됨
 
     def draw(self):
-        draw_rectangle(* self.get_bb())
+        # 디버그용 바운딩박스
+        draw_rectangle(*self.get_bb())
 
     def update(self):
+        # 일정 시간이 지나면 자동 삭제
         if get_time() > self.end_time:
-            game_world.remove_object(self)
+            game_world.remove_collision_object(self)
 
     def get_bb(self):
+        # owner의 위치 + 오프셋 적용
         x = self.owner.x + self.offset_x
         y = self.owner.y + self.offset_y
-        return x - self.w/2, y - self.h/2, x + self.w/2, y + self.h/2
+        return (
+            x - self.w / 2,
+            y - self.h / 2,
+            x + self.w / 2,
+            y + self.h / 2
+        )
 
-    def handle_collision(self, other, group):
+    def handle_collision(self, group, other):
+        # 충돌 시 HitBox 자체는 특별한 로직 없음
+        # 피해 계산은 Boxer.handle_collision에서 처리함
         pass
