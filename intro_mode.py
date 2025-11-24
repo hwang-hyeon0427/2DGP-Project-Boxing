7177203f6b3235540ea9cfbe6d7b7bcd95a8e8a5
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-25 02:07:27 +0900

    Update intro_mode.py

--- intro_mode.py
+++ intro_mode.py
@@ -4,6 +4,18 @@ import title_mode
 
 image= None
 
+frame = 0.0
+FRAME_COUNT = 8
+FRAME_ROWS = 3
+TARGET_ROWS = 1
+
+ANIMATION_SPEED = 10.0
+
+FRAME_W = 2237 // FRAME_COUNT
+FRAME_H = 737 // FRAME_ROWS
+
+logo_start_time = 0
+
 
 def init():
     pass
@@ -17,9 +29,7 @@ def update():
 def draw():
     pass
 
-def handle_events():
-    pass
-
+def handle_events(): pass
 def pause(): pass
 def pause(): pass
 def resume(): pass
