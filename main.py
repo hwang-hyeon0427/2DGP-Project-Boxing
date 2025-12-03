b1329c71f3e7c20bff503a07b9d25bd0e01fbff4
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-12-04 02:20:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pico2d import *
-import logo_mode as start_mode
+import play_mode as start_mode
 import game_framework
 
 
-open_canvas(317*4, 128*5)
+open_canvas(317*3, 128*5)
 game_framework.run(start_mode)
 close_canvas()
