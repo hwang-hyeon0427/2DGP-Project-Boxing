40589578992894d8c7e5615a247cddd223edc5df
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-12-04 02:19:49 +0900

    Update attack_state.py

--- attack_state.py
+++ attack_state.py
@@ -1,56 +1,59 @@
-from pico2d import get_time
+from pico2d import *
 import game_framework
+from hitbox_data import HITBOX_DATA
 
 class AttackState:
     def __init__(self, boxer, attack_type):
-        self.b = boxer
-        self.type = attack_type
+        """attack_type: 'front_hand', 'rear_hand', 'uppercut'"""
+        self.boxer = boxer
+        self.attack_type = attack_type
+
         self.done = False
+        self.total_frames = 0
+        self.hit_frames = []
         self.spawned_frames = set()
 
     def enter(self, e):
-        sheet = self.b.cfg[self.type]
-        self.b.use_sheet(sheet)
+        sheet = self.boxer.cfg[self.attack_type]
+        self.boxer.use_sheet(sheet)
 
-        self.b.frame = 0
+        self.boxer.frame = 0
         self.done = False
         self.spawned_frames.clear()
 
-        self.b.is_attacking = True
-        self.b.current_attack_type = self.type
-
+        self.total_frames = sheet['cols']
         self.hit_frames = sheet.get('hit_frames', [])
 
-        self.b.last_attack_time = get_time()
+        self.boxer.current_attack_type = self.attack_type
+        self.boxer.is_attacking = True
 
     def exit(self, e):
-        self.b.is_attacking = False
-        self.b.current_attack_type = None
+        self.boxer.current_attack_type = None
+        self.boxer.is_attacking = False
 
     def do(self):
         if self.done:
             return
 
-        self.b.frame += self.b.FRAMES_PER_ACTION * self.b.ACTION_PER_TIME * game_framework.frame_time
+        # ★ 프레임 속도 (초당 10프레임 재생 정도)
+        frame_speed = 10 * game_framework.frame_time
 
-        cur_frame = int(self.b.frame)
+        # 프레임 증가
+        self.boxer.frame += frame_speed
+        cur_frame = int(self.boxer.frame)
 
-        if cur_frame in self.hit_frames and cur_frame not in self.spawned_frames:
-            self.b.spawn_hitbox()
+        # ★ HITBOX_DATA 기반 hit frame 처리
+        from hitbox_data import HITBOX_DATA
+        hitbox_info = HITBOX_DATA[self.boxer.controls][self.attack_type]
+
+        if cur_frame in hitbox_info and cur_frame not in self.spawned_frames:
+            self.boxer.spawn_hitbox()
             self.spawned_frames.add(cur_frame)
 
-        if self.b.frame >= self.b.cols:
+        # 애니메이션 종료
+        if cur_frame >= self.total_frames:
             self.done = True
-            self.b.state_machine.handle_state_event(('ANIMATION_END', None))
+            self.boxer.state_machine.handle_state_event(('STOP', None))
 
     def draw(self):
-        self.b.frame = float(self.b.frame)  # 안전장치 (optional)
-        self.b.image.clip_draw(
-            int(self.b.frame) * self.b.frame_w,
-            0,
-            self.b.frame_w,
-            self.b.frame_h,
-            self.b.x,
-            self.b.y
-        )
-
+        self.boxer.draw_current()
