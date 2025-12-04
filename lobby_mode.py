f1875845140be76a0bb853857db80eca68674c2b
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-12-05 01:44:58 +0900

    Update lobby_mode.py

---
+++ lobby_mode.py
@@ -0,0 +1,75 @@
+import game_world
+import play_mode
+import game_world
+import game_framework
+
+from pico2d import *
+from boxing_ring import BoxingRing
+from button import Button
+
+def start_game():
+    game_framework.change_state(play_mode)
+
+def how_to_play():
+    pass
+
+def exit_game():
+    game_framework.quit()
+
+def init():
+    global bg, start_btn, how_btn, exit_btn
+
+    game_world.clear()
+
+    bg = BoxingRing()
+    game_world.add_object(bg, 0)
+
+    start_btn = Button(400, 300, 250, 80,
+                       'ui/start_normal.png',
+                       'ui/start_hover.png',
+                       on_click=start_game)
+
+    how_btn = Button(400, 200, 250, 80,
+                     'ui/how_normal.png',
+                     'ui/how_hover.png',
+                     on_click=how_to_play)
+
+    exit_btn = Button(400, 100, 250, 80,
+                      'ui/exit_normal.png',
+                      'ui/exit_hover.png',
+                      on_click=exit_game)
+
+    game_world.add_object(start_btn, 1)
+    game_world.add_object(how_btn, 1)
+    game_world.add_object(exit_btn, 1)
+
+
+def update():
+    game_world.update()
+
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+
+def handle_events():
+    events = get_events()
+    for e in events:
+        if e.type == SDL_QUIT:
+            exit_game()
+
+        # 버튼 이벤트 전달
+        for obj in game_world.world[1]:
+            obj.handle_event(e)
+
+        if e.type == SDL_KEYDOWN and e.key == SDLK_ESCAPE:
+            exit_game()
+
+
+def finish():
+    game_world.clear()
+
+def pause(): pass
+def resume(): pass
