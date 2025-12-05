2c2b8c7ab36f487805d510fff0f98861299da97b
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-12-06 02:39:05 +0900

    SpriteSheetButton ==> Button

--- lobby_mode.py
+++ lobby_mode.py
@@ -3,17 +3,13 @@ import game_framework
 import game_world
 import play_mode
 
-from button import SpriteSheetButton
+from button import Button
 from boxing_ring import BoxingRing   # ← 최적화된 배경 클래스
 import mouse
 
 
 background = None
 buttons = []
-
-def levels():
-    print("LEVELS")
-    game_framework.change_mode(lobby_mode)
 
 def start_game():
     print("START GAME")
@@ -24,15 +20,12 @@ def init():
 
     game_world.clear()
 
-    # -----------------------------
-    # 애니메이션 배경 BoxingRing
-    # -----------------------------
     background = BoxingRing()
     game_world.add_object(background, 0)   # depth 0에 배경 추가
 
     sheet = "resource/buttons_spritesheet_Photoroom.png"
 
-    start_btn = SpriteSheetButton(
+    start_btn = Button(
         sheet_path = sheet,
         row = 9,            # START 버튼 row
         x = 400, y = 300,
@@ -40,7 +33,7 @@ def init():
         on_click=start_game
     )
 
-    # back_btn = SpriteSheetButton(
+    # back_btn = Button(
     #     sheet_path=sheet,
     #     row=1,            # BACK 버튼 row
     #     x=400, y=200,
