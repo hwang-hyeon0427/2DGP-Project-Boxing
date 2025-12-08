2cb53dc50b46625d59d02f97ddf78688762bef4f
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-12-08 20:08:04 +0900

    Update levels_mode.py

--- levels_mode.py
+++ levels_mode.py
@@ -1,33 +1,37 @@
 from pico2d import *
 from button import SpriteSheetButton
-
-import game_framework
-import game_world
 from mouse import update as mouse_update
 from mouse import get_pos as mouse_get_pos
 
+import game_framework
+import game_world
 
+selected_level = None
 buttons = []
 
-def level_selected(level_no):
-    print("LEVEL:", level_no)
-    # 필요하면 play_mode로 이동
-    # game_framework.change_mode(play_mode)
 
 def init():
-    global buttons
+    global buttons, selected_level
     game_world.clear()
+
+    selected_level = None
 
     sheet = "resource/buttons_spritesheet_Photoroom.png"
 
     buttons = [
         SpriteSheetButton(sheet, row=3, x=400, y=400, scale=6,
-                          on_click=lambda: level_selected(1)),
+                          on_click=lambda: level_selected(1)), # Level easy
         SpriteSheetButton(sheet, row=4, x=400, y=300, scale=6,
-                          on_click=lambda: level_selected(2)),
+                          on_click=lambda: level_selected(2)), # Level medium
         SpriteSheetButton(sheet, row=5, x=400, y=200, scale=6,
-                          on_click=lambda: level_selected(3)),
+                          on_click=lambda: level_selected(3)), # Level hard
     ]
+
+def level_selected(level_no):
+    global selected_level
+    selected_level = level_no
+    print("LEVEL SELECTED:", selected_level)
+    game_framework.change_mode(character_select_mode)
 
 def update():
     mx, my = mouse_get_pos()
@@ -45,8 +49,10 @@ def handle_events():
     for e in events:
         if e.type == SDL_QUIT:
             game_framework.quit()
+        elif e.type == SDL_KEYDOWN and e.key == SDLK_ESCAPE:
+            game_framework.quit()
         elif e.type == SDL_MOUSEMOTION:
-            mouse_update(events)
+            mouse_update(e)
         elif e.type == SDL_MOUSEBUTTONDOWN:
             mx, my = mouse_get_pos()
             for b in buttons:
