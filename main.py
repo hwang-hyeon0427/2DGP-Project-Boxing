53e5ad20f239c0c52292ff27280eac5d53da775c
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-12-08 22:31:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from pico2d import *
+from ctypes import windll
+
 import play_mode as start_mode
 import game_framework
-from ctypes import windll
+import sound_manager
+
 
 # Windows에서 화면 해상도 가져옴 (DPI 보정 포함)
 user32 = windll.user32
@@ -11,6 +14,8 @@ screen_h = user32.GetSystemMetrics(1)
 
 
 open_canvas(screen_w, screen_h)
+
+sound_manager.load()
 
 game_framework.run(start_mode)
 close_canvas()
