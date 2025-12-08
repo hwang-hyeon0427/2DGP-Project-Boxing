49fdf0afdec791837f141c3486c548041230504c
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-12-09 04:43:34 +0900

    Update state_machine.py

--- state_machine.py
+++ state_machine.py
@@ -1,4 +1,6 @@
 from event_to_string import event_to_string
+from debug_manager import log, DEBUG_STATE
+
 
 class StateMachine:
     def __init__(self, start_state, rules: dict):
@@ -18,11 +20,11 @@ class StateMachine:
                 self.next_state = self.rules[self.cur_state][check_event]
                 self.cur_state.exit(state_event)
                 self.next_state.enter(state_event)
-                print(f'State Change: {self.cur_state.__class__.__name__} ======= {event_to_string(state_event)}======= {self.next_state.__class__.__name__}')
+                log(DEBUG_STATE,f'State Change: {self.cur_state.__class__.__name__} ======= {event_to_string(state_event)}======= {self.next_state.__class__.__name__}')
                 self.cur_state = self.next_state
                 return
         if state_event[0] == 'INPUT':
             return
-        print(f'처리되지 않은 이벤트 {event_to_string(state_event)}가 있습니다')
+        log(DEBUG_STATE,f'처리되지 않은 이벤트 {event_to_string(state_event)}가 있습니다')
 
 
